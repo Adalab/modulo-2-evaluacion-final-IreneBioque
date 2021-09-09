2002a933adb6ed6b1b268b6d00961c588b1d5c06
IreneBioque <irenebioque@gmail.com>
2021-09-09 21:27:37 +0200

    add functions input

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('.js_search');
+const button = document.querySelector('.js_button');
+const favourite = document.querySelector('.js_favourite');
+const form = document.querySelector('.js_form');
+let data = [];
+
+fetch(`//api.tvmaze.com/search/shows?q=:${input.value}`)
+  .then( response => response.json() )
+  .then( dataApi => {
+    data = dataApi;
+
+    // render();
+  } );
+
+function render() {
+  console.log(data);
+  const dataFilter = data
+      .filter(data => data.name.toLocaleLowerCase().includes(input.value))
+    .map(serieData => serieData.name)
+  favourite.innerHTML = '';
+  for (const data of dataFilter) {
+    const html = `<li>${data}</li>`;
+
+    favourite.innerHTML += html;
+  }
+}
+
+function handleType() {
+  render();
+}
+
+input.addEventListener('keyup', handleType);
+form.addEventListener('submit', (event) => event.preventDefault() );
+// function handleSearch(){
+//   console.log(inputSearch);
+// }
+
+
+
+// button.addEventListener('click', handleSearch);
+
+
+
