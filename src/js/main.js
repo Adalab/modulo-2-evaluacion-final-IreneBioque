50debb9cb62427b1e5efbada1cc6a72b79a93cad
IreneBioque <irenebioque@gmail.com>
2021-09-10 21:51:03 +0200

    add selected show style

--- src/js/main.js
+++ src/js/main.js
@@ -37,16 +37,11 @@ function url() {
 
 function paintHtml() {
   console.log(shows);
+  list.innerHTML = '';
   let favClass = '';
-
+  
   for (const serie of shows) {
-    //  debugger;
-    let isValidClass;
-    if (isValidShow(serie)) {
-      isValidClass = '';
-    } else {
-      isValidClass = 'hidden';
-    }
+      // debugger;
     const isFav = isFavorite(serie);
     if (isFav) {
       favClass = 'selected';
@@ -58,11 +53,11 @@ function paintHtml() {
     const nullImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
     const idli = serie.id;
     if (showImageNull === null) {
-      const html = `<li class="js_li ${favClass} ${isValidClass}" id="${idli}"><h3>${showName}</h3><img src="${nullImage}" alt="covershow"></li>`;
+      const html = `<li class="js_li ${favClass}" id="${idli}"><h3>${showName}</h3><img src="${nullImage}" alt="covershow"></li>`;
       list.innerHTML += html;
     } else {
       const showImage = serie.image.medium;
-      const html = `<li class="js_li" id="${idli}"><h3>${showName}</h3><img src="${showImage}" alt="covershow"></li>`;
+      const html = `<li class="js_li ${favClass}" id="${idli}"><h3>${showName}</h3><img src="${showImage}" alt="covershow"></li>`;
       list.innerHTML += html;
     }
   }
@@ -83,9 +78,9 @@ function handleShow(ev) {
   } else {
     favorites.splice(favoritesFound, 1);
   }
-
-
+ 
   console.log(favorites);
+  paintHtml();
   paintfavorites();
   setInLocalStorage();
 }
@@ -99,11 +94,6 @@ function isFavorite(show) {
   } else {
     return true;
   }
-}
-
-function isValidShow(show) {
-  const filterValue = input.value;
-  return show.name.toLowerCase().includes(filterValue);
 }
 
 
