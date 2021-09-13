6c37d4fe52146cd8c2810ec19f4d09cc58213aaa
IreneBioque <irenebioque@gmail.com>
2021-09-13 10:15:12 +0200

    add partials js

---
+++ src/js/00variables.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const input = document.querySelector('.js_search');
+const button = document.querySelector('.js_button');
+const list = document.querySelector('.js_list');
+const favoritesList = document.querySelector('.js_favorites');
+const buttonReset = document.querySelector('.js_reset');
+let shows = [];
+let favorites = [];
---
+++ src/js/03favorites.js
@@ -0,0 +1,33 @@
+
+function paintfavorites() {
+    favoritesList.innerHTML = '';
+  
+    for (let favorite of favorites) {
+      const title = favorite.title;
+      const showImageNull = favorite.image;
+      const id = favorite.id;
+      const nullImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+      if (showImageNull === null) {
+        const html = `<li class="js_listfavorite lifavorite" id="${id}"><img src="${nullImage}" alt="covershow" class="lifavorite__image"><h3 class="lifavorite__title">${title}</h3><i class="fas fa-times-circle js_deleted lifavorite__deleted" id="${id}"></li>`;
+        favoritesList.innerHTML += html;
+      } else {
+        const image = favorite.image.medium;
+        const html = `<li class="js_listfavorite lifavorite" id="${id}"><img src="${image}" alt="covershow" class="lifavorite__image"><h3 class="lifavorite__title">${title}</h3 class="lifavorite__title"><i class="fas fa-times-circle js_deleted lifavorite__deleted" id="${id}"></i></li>`;
+        favoritesList.innerHTML += html;
+      }
+    }
+    listenCloses();
+  }
+  function listenShows() {
+    const listShows = document.querySelectorAll('.js_li');
+    for (let li of listShows) {
+      li.addEventListener('click', handleShow);
+    }
+  }
+  
+  function handleType(ev) {
+    ev.preventDefault();
+    url();
+  }
+  
+  button.addEventListener('click', handleType);
