f4a40e29c574ebf13b3abbdcf69713daffb66116
IreneBioque <irenebioque@gmail.com>
2021-09-13 09:50:19 +0200

    add style

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ const input = document.querySelector('.js_search');
 const button = document.querySelector('.js_button');
 const list = document.querySelector('.js_list');
 const favoritesList = document.querySelector('.js_favorites');
-// const form = document.querySelector('.js_form');
 const buttonReset = document.querySelector('.js_reset');
 let shows = [];
 let favorites = [];
